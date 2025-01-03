263f350ee0366b914ad4a8d414a62ab75c3310e3
Andrii Khymera <andriikhymera@gmail.com>
2025-01-03 15:09:11 +0200

    Add/fix nvim-cmp mapping for arrow keys

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -7,4 +7,8 @@ local map = vim.keymap.set
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
---get_projects map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+--get_projects map({ "n", "i", "v" }, "<C-s>", "<cmd> wcr>")
+
+--nvim-cmp arrow keys mapping in sugessions. From some reason these doesn't work by defoult. Maybe there is some conflict, but imap doesn't show anything
+map('i', '<Up>', require('cmp').mapping.select_prev_item())
+map('i', '<Down>',require('cmp').mapping.select_next_item())
