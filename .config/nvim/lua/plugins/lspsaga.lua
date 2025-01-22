5bf3ac950229c549873c64eabda2bef441d83c9c
Andrii Khymera <andriikhymera@gmail.com>
2025-01-22 12:58:06 +0200

    Use the lspsaga instead of aerial

---
+++ .config/nvim/lua/plugins/lspsaga.lua
@@ -0,0 +1,15 @@
+return {
+    'nvimdev/lspsaga.nvim',
+    event = 'LspAttach',
+    config = function()
+        require('lspsaga').setup({})
+    end,
+    dependencies = {
+        'nvim-treesitter/nvim-treesitter', -- optional
+        'nvim-tree/nvim-web-devicons',     -- optional
+    },
+    keys = {
+      { "<leader>o", "<cmd>Lspsaga outline<CR>", desc = "Lspsaga outline" }
+    }
+}
+
