acba642c9ccc53e6008b7cfca3e001d8e763f438
Andrii Khymera <andriikhymera@gmail.com>
2025-02-06 14:47:19 +0200

    Add nvim-ts-autotag for XML

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,7 +1,4 @@
 return {
     "tpope/vim-surround",
     event = "BufEnter",
-    config = function()
-        require("nvim-surround").setup()
-    end
 }
---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,8 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = { "xml"},
+    highlight = { enable = true },
+    indent = { enable = true },
+  },
+}
