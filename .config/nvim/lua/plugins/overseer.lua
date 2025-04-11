0a83bfe442f47b644b43cd9ef344c9059bc50524
Andrii Khymera <andriikhymera@gmail.com>
2025-04-11 21:40:17 +0300

    Add the cmp config for tab, lua. Add lazydev

--- .config/nvim/lua/plugins/overseer.lua
+++ .config/nvim/lua/plugins/overseer.lua
@@ -1,3 +1,11 @@
 return {
   'stevearc/overseer.nvim',
+
+  config = function ()
+    require("overseer").setup({
+      templates = { "builtin", "make" },
+      task_dirs = { ".", ".idea" }
+    })
+  end
+
 }
