f3e0f4c7f0ae26e18728ef42520dfd3d2ec1b8da
Andrii Khymera <andriikhymera@gmail.com>
2025-04-18 20:31:16 +0300

    Fix live grep and find files in hidden dirs excluding .git/

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -7,11 +7,7 @@ return{
   },
   dependencies = {
     "mistweaverco/uzuri.nvim",  -- Ensures uzuri loads first
-    -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
     "MunifTanjim/nui.nvim",
-    -- OPTIONAL:
-    --   `nvim-notify` is only needed, if you want to use the notification view.
-    --   If not available, we use `mini` as the fallback
     "rcarriga/nvim-notify",
     }
 }
--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,9 +3,29 @@ return {
   dependencies = { 'nvim-lua/plenary.nvim'},
   config = function ()
     require("telescope").setup({
+      defaults = {
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden",         -- this includes hidden files
+          "--no-ignore",      -- overrides .gitignore
+          "--glob=!**/.git/*" -- avoids noisy .git
+        },
+      },
       pickers = {
         find_files = {
           hidden = true, -- Enable hidden file searching
+          find_command = {
+            "fd",
+            "--type", "f",
+            "--hidden",
+            "--exclude", ".git"
+          }
         },
       },
       keys = {
