da92a42cecd53481c6877a1dc57749c656e0ba12
Andrii Khymera <andriikhymera@gmail.com>
2025-04-25 13:08:37 +0300

    Add the inc-rename

---
+++ .config/nvim/lua/plugins/inc-rename.lua
@@ -0,0 +1,11 @@
+return {
+  "smjonas/inc-rename.nvim",
+  lazy = false,
+  config = function()
+    require("inc_rename").setup()
+  end,
+  keys = {
+    -- Shift + F6 
+    { "<80>F8", ":IncRename", desc = "IncRename" }
+  }
+}
