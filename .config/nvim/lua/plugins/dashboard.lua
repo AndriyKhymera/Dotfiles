6c4e3b8e78c45b2b534d3389442f945f325f1497
Andrii Khymera <andriikhymera@gmail.com>
2025-04-25 13:08:29 +0300

    Fix the version of dashboards before breaking commit, add the shortcut for projects

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -1,13 +1,33 @@
 return {
   'nvimdev/dashboard-nvim',
+  commit = "74e3b981a49dbbd0b901531a21495ddd06af19c9",
   event = 'VimEnter',
-  dependencies = { 'nvim-tree/nvim-web-devicons' },
-  config = {
-    week_header = {
-      enable = true,
-      -- concat  --concat string after time string line
-      -- append  --table append after time string line
-    },
-    -- disable_move  -- boolean default is false disable move key
-  }
+  config = function()
+    require('dashboard').setup {
+      theme = 'hyper',
+      config = {
+        week_header = {
+          enable = true,
+        },
+        shortcut = {
+          { desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u' },
+          {
+            icon = ' ',
+            icon_hl = '@variable',
+            desc = 'Files',
+            group = 'Label',
+            action = 'Telescope find_files',
+            key = 'f',
+          },
+          {
+            desc = ' projects',
+            group = 'Number',
+            action = 'Telescope projects',
+            key = 'p',
+          },
+        },
+      },
+    }
+  end,
+  dependencies = { { 'nvim-tree/nvim-web-devicons' } }
 }
