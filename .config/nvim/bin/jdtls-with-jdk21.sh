fc4dc8503bc67073e44bf7da6f46f47d5cab0135
Andrii Khymera <andriikhymera@gmail.com>
2025-06-19 13:09:11 +0300

    Fix jdtls start after version update

--- .config/nvim/bin/jdtls-with-jdk21.sh
+++ .config/nvim/bin/jdtls-with-jdk21.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-JAVA_HOME="$HOME/.sdkman/candidates/java/21.0.5-amzn" exec "/opt/homebrew/Cellar/jdtls/1.44.0/libexec/bin/jdtls"  "$@"
+current_jdtls_version=$(ls /opt/homebrew/Cellar/jdtls/)
+JAVA_HOME="$HOME/.sdkman/candidates/java/21.0.5-amzn" exec "/opt/homebrew/Cellar/jdtls/${current_jdtls_version}/libexec/bin/jdtls"  "$@"
