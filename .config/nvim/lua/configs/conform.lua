f3b070c1cdb4003ba2242dffea0b2338d99ecb94
Andrii Khymera <andriikhymera@gmail.com>
2025-12-01 12:34:36 +0200

    Add the formating for java from google, add remote sshfs plugin

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -5,8 +5,15 @@ local options = {
     html = { "prettier" },
     json = { "jq" },
     hocon = { "jq" },
+    java = { "google-java-format" },
   },
-
+  formatters = {
+    ["google-java-format"] = {
+      command = "google-java-format",
+      args = { "-" },
+      stdin = true,
+    },
+  },
   -- format_on_save = {
   --   -- These options will be passed to conform.format()
   --   timeout_ms = 500,
