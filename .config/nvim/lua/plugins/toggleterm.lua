1cc19edc910a31ab52afcf47ca1d0f7daebe75be
Andrii Khymera <ext-andrii.khymera@here.com>
2026-04-09 21:58:00 +0300

    Add toggle term for k9s

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -2,9 +2,31 @@ return {
   "akinsho/toggleterm.nvim",
   version = "*",
   keys = {
-    { [[<c-\>]], desc = "Toggle terminal" },
+    { [[<c-\>]], mode = { "n", "t" }, desc = "Toggle terminal" },
+    { "<leader>k", desc = "Toggle k9s" },
   },
-  opts = {
-    open_mapping = [[<c-\>]],
-  },
+  config = function()
+    require("toggleterm").setup({
+      open_mapping = [[<c-\>]],
+    })
+
+    -- close terminal from within terminal mode in one step
+    vim.keymap.set("t", [[<c-\>]], "<cmd>ToggleTerm<cr>", { desc = "Close terminal" })
+
+    local Terminal = require("toggleterm.terminal").Terminal
+    local k9s = Terminal:new({
+      cmd = "k9s",
+      hidden = true,
+      direction = "float",
+      on_open = function(term)
+        vim.keymap.set("t", [[<c-\>]], function()
+          term:toggle()
+        end, { buffer = term.bufnr, desc = "Close k9s terminal" })
+      end,
+    })
+
+    vim.keymap.set("n", "<leader>k", function()
+      k9s:toggle()
+    end, { desc = "Toggle k9s" })
+  end,
 }
