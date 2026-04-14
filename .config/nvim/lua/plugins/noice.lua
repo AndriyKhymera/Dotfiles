8035992b698b762f6fd020e340646b5022eeaca6
Andrii Khymera <ext-andrii.khymera@here.com>
2026-04-14 22:00:18 +0300

    Noice shorcuts, messages in separate window, add shortcuts, load noice on UIEnter

--- .config/nvim/lua/plugins/noice.lua
+++ .config/nvim/lua/plugins/noice.lua
@@ -1,8 +1,22 @@
 return {
   "folke/noice.nvim",
-  event = "VeryLazy",
+  event = "UIEnter",
   opts = {
-    -- add any options here
+    messages = {
+      view_history = "messages", -- :messages opens noice split
+    },
+    views = {
+      messages = {
+        backend = "split",
+        enter = true,
+        size = "40%",
+      },
+    },
+  },
+  keys = {
+    { "<leader>mn", "<cmd>Noice history<cr>", desc = "Message history" },
+    { "<leader>ml", "<cmd>Noice last<cr>", desc = "Last message" },
+    { "<leader>md", "<cmd>Noice dismiss<cr>", desc = "Dismiss notifications" },
   },
   dependencies = {
     "mistweaverco/uzuri.nvim", -- Ensures uzuri loads first
