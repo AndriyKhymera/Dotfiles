9b0fe64b2fe051ad694acc67b5f7fe3aca06c064
Andrii Khymera <andriikhymera@gmail.com>
2026-04-16 13:28:49 +0300

    Delete conform config for Java

--- .config/nvim/lua/configs/conform.lua
+++ .config/nvim/lua/configs/conform.lua
@@ -5,25 +5,8 @@ local options = {
     html = { "prettier" },
     json = { "jq" },
     hocon = { "jq" },
-    java = { "google-java-format" },
-  },
-  formatters = {
-    ["google-java-format"] = {
-      command = vim.fn.expand("~/.dotfiles/.config/nvim/bin/google-java-format-java21.sh"),
-      args = { "-" },
-      stdin = true,
-    },
-  },
-  keys = {
-    {
-      "<leader>fr",
-      function()
-        require("conform").format({ async = true })
-      end,
-      mode = { "n" },
-      desc = "Conform: Format file"
-    }
   },
 }
+
 
 return options
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -3,16 +3,6 @@ return {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
-    keys = {
-      {
-        "<leader>fr",
-        function()
-          require("conform").format({ async = true })
-        end,
-        mode = { "n" },
-        desc = "Format file"
-      }
-    },
   },
 
   -- These are some examples, uncomment them if you want to see them work!
