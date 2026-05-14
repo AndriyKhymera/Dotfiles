85c29b520051bcafaad0ddd71820a4920198f5ca
Andrii Khymera <andriikhymera@gmail.com>
2026-05-14 14:37:57 +0300

    Enable project local files

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,6 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Enable project-local .nvim.lua files (Neovim will prompt to trust on first load)
+vim.o.exrc = true
